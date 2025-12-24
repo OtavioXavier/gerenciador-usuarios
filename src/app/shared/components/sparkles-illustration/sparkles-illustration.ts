0294be6f48f8c31ab45554c4090d6ae136932305
OtavioXavier <otavioxaviermarinho@gmail.com>
2025-12-24 10:55:27 -0300

    ✨ feat[shared]: add sparkles illustration component

---
+++ src/app/shared/components/sparkles-illustration/sparkles-illustration.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sparkles-illustration',
+  imports: [],
+  templateUrl: './sparkles-illustration.html',
+  styleUrl: './sparkles-illustration.scss',
+})
+export class SparklesIllustration {}
+
